39e9891f46947f66c46cd5edcf87caf35dc2cc9a
Acosta1984 <102642810+Acosta1984@users.noreply.github.com>
2025-02-02 13:24:14 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // Permite acesso externo
+    port: 5173,       // Garante que a porta correta seja usada
+    strictPort: true, // Garante que a porta 5173 será usada, senão erro
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
